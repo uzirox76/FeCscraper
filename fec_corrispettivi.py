01c17107982cd8c3d606bdcd825ab42b521c28e8
socrat3 <salvatore.crapanzano@gmail.com>
2021-03-31 13:35:09 +0200

    Sistemato l'invio per singola ditta
    Sistemato l'invio per singola ditta

--- fec_corrispettivi.py
+++ fec_corrispettivi.py
@@ -1,6 +1,6 @@
 ## Crapanzano Salvatore (Corrispettivi Fattura)
 ## Per effettuare l'invio delle richieste corrispettivi sul portale ivaservizi.agenziaentrate.gov.it, inserire il nome del file nella relativa parte del codice
-## Vers. 1.8.2 del 18-03-2021
+## Vers. 1.9.0 del 31-03-2021
 ## Cartella di lavoro è ./inviocorrispettivi sotto la dir di lancio dello script di invio
 import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
@@ -40,6 +40,7 @@ cfcliente = sys.argv[7]
 pivadiretta  = sys.argv[8]
 path = r'inviocorrispettivi'
 
+nr_file = 0 
 if not os.path.exists(path):
     os.makedirs(path)
 
@@ -97,7 +98,7 @@ def FileCorrispettivi_scrivi(filename, lines, debug_len=False):
         print("File length:  precomp_payload=%d\tI/O=%d"%(payload_len,file_len))
         sys.exit(0)
 cfclientexml = path + '/' + 'COR_' + cfcliente + '.xml'
-
+cfclientexmlnopath = 'COR_' + cfcliente + '.xml' 
 
 #uso della funzione che crea il file con il definizione del nome file xml e ontenuto
 FileCorrispettivi_scrivi(cfclientexml, file_corrispettivi)
@@ -200,67 +201,64 @@ print('Accetto le condizioni')
 r = s.get('https://ivaservizi.agenziaentrate.gov.it/cons/cons-services/rs/disclaimer/accetta?v='+unixTime() , headers = headers_token )
 cookieJar = s.cookies
 
-#print('controllo file')
-#r = s.get('https://ivaservizi.agenziaentrate.gov.it/ser/fatturewizard/html/partials/supporto/controllo.html?v='+now.strftime("%Y-%m-%d"), headers = headers)
 
 #seleziona file 
 ## nome_file='IT02081640845_00001.xml'
 nr_file = 0
-for root, dirs, files in os.walk("./inviocorrispettivi"):
-    for file in files:
-        print("Elenco file Corrispettivi da inviare: ", files)
-        ext = [".xml", "XML"]
-        if file.endswith(tuple(ext)):
-             nome_file = file
-             # print("Percorso", os.path.join(dirs))
-             print("Il nome file da inviare è", nome_file)
-             with open('./inviocorrispettivi/' + nome_file, "rb") as f:
-                 headers_file= { 'Host': 'ivaservizi.agenziaentrate.gov.it',
-                                 'Cache-Control': 'max-age=0',
-                                 'sec-ch-ua': '"Chromium";v="88", "Google Chrome";v="88", ";Not A Brand";v="99"',
-                                 'sec-ch-ua-mobile': '?0',
-                                 'Sec-Fetch-Site': 'same-origin',
-                                 'Sec-Fetch-Mode': 'cors',
-                                 'Sec-Fetch-User': '?1',
-                                 'Sec-Fetch-Dest': 'empty',
-                                 'Upgrade-Insecure-Requests': '1',
-                                 'accept': 'application/json, text/plain, */*',
-                                 'accept-encoding': 'gzip, deflate',
-                                 'accept-language': 'it-IT,it;q=0.9,en-US;q=0.8,en;q=0.7,fr;q=0.6',
-                                 'Origin': 'https://ivaservizi.agenziaentrate.gov.it',
-                                 'x-frame-options': 'deny',
-                                 'x-b2bcookie': xb2bcookie,
-                                 'x-token': xtoken,
-                                 'X-XSS-Protection': '1; mode=block',
-                                 'Strict-Transport-Security': 'max-age=16070400; includeSubDomains',
-                                 'Content-Type': 'application/xml;charset=utf-8',
-                                 'connection': 'keep-alive',
-                                 'X-Frame-Options': 'deny',
-                                 'x-nome-file': nome_file,
-                                 'X-Content-Type-Options': 'nosniff',
-                                 'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36'}    
+print("Elenco file Corrispettivi da inviare: ", cfclientexmlnopath)##
+ext = [".xml", ".XML"]
+if cfclientexmlnopath.endswith(tuple(ext)):
+     nome_file = cfclientexmlnopath
+     # print("Percorso", os.path.join(dirs))
+     print("Il nome file da inviare è", nome_file)
+     with open('./inviocorrispettivi/' + nome_file, "rb") as f:
+         headers_file= { 'Host': 'ivaservizi.agenziaentrate.gov.it',
+                         'Cache-Control': 'max-age=0',
+                         'sec-ch-ua': '"Chromium";v="88", "Google Chrome";v="88", ";Not A Brand";v="99"',
+                         'sec-ch-ua-mobile': '?0',
+                         'Sec-Fetch-Site': 'same-origin',
+                         'Sec-Fetch-Mode': 'cors',
+                         'Sec-Fetch-User': '?1',
+                         'Sec-Fetch-Dest': 'empty',
+                         'Upgrade-Insecure-Requests': '1',
+                         'accept': 'application/json, text/plain, */*',
+                         'accept-encoding': 'gzip, deflate',
+                         'accept-language': 'it-IT,it;q=0.9,en-US;q=0.8,en;q=0.7,fr;q=0.6',
+                         'Origin': 'https://ivaservizi.agenziaentrate.gov.it',
+                         'x-frame-options': 'deny',
+                         'x-b2bcookie': xb2bcookie,
+                         'x-token': xtoken,
+                         'X-XSS-Protection': '1; mode=block',
+                         'Strict-Transport-Security': 'max-age=16070400; includeSubDomains',
+                         'Content-Type': 'application/xml;charset=utf-8',
+                         'connection': 'keep-alive',
+                         'X-Frame-Options': 'deny',
+                         'x-nome-file': nome_file,
+                         'X-Content-Type-Options': 'nosniff',
+                         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36'}    
 
-                 print("Contenuto", f)    
-                 r = s.post('https://ivaservizi.agenziaentrate.gov.it/cons/mass-services/rs/file/upload?tipoRichiesta=CORR',headers=headers_file, data=f)
-                 cookieJar = s.cookies
-                 print(r.status_code)
-                 if r.status_code == 500:
-                     print('Servizio dell''Agenzia delle Entrate non disponibile')
-                     print('Invio non andanto a buon fine, riprovare più tardi')
-                     print('Disconnessione in corso')
-                     r = s.get('https://ivaservizi.agenziaentrate.gov.it/portale/c/portal/logout')
-                     sys.exit()
-                 if r.status_code == 200:
-                     print('Invio file/files Corrispettivi inviato/i con successo!', nome_file)
-                     print("Codice invio Corrispettivi:", r.text)
-                     conservate = open(cfcliente+ "_corrispettivi_inviati.csv", 'a')
-                     conservate.write(nome_file)
-                     conservate.write(",")
-                     conservate.write(r.text)
-                     conservate.write(",")
-                     conservate.write(str(now) + "\n")
-                     conservate.close()
-                     nr_file = nr_file + 1
-        print("nr. files inviato/i --> ", nr_file)
-        os.rename(cfclientexml, cfclientexml + now.strftime("%Y-%m-%d") + '.xml' + 'inviato')
-        continue;
+         print("Contenuto", f)    
+         r = s.post('https://ivaservizi.agenziaentrate.gov.it/cons/mass-services/rs/file/upload?tipoRichiesta=CORR',headers=headers_file, data=f)
+         cookieJar = s.cookies
+         print(r.status_code)
+         if r.status_code == 500:
+             print('Servizio dell''Agenzia delle Entrate non disponibile')
+             print('Invio non andanto a buon fine, riprovare più tardi')
+             print('Disconnessione in corso')
+             r = s.get('https://ivaservizi.agenziaentrate.gov.it/portale/c/portal/logout')
+             sys.exit()
+         if r.status_code == 200:
+             print('Invio file/files Corrispettivi inviato/i con successo!', nome_file)
+             print("Codice invio Corrispettivi:", r.text)
+             conservate = open(cfcliente+ "_corrispettivi_inviati.csv", 'a')
+             conservate.write(nome_file)
+             conservate.write(",")
+             conservate.write(r.text)
+             conservate.write(",")
+             conservate.write(str(now) + "\n")
+             conservate.close()
+             nr_file = nr_file + 1
+             time.sleep(5)
+             print("nr. files inviato/i --> ", nr_file)
+             r = s.get('https://ivaservizi.agenziaentrate.gov.it/portale/c/portal/logout')
+os.rename(cfclientexml, cfclientexml  + '_inviato_il_' + now.strftime("%Y-%m-%d"))
